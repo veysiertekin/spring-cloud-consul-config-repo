dd078f0f04a922a262c5bad6dc8d69cfd2a84ce8
Ertekin, Veysi <vertekin@ebay.com>
2017-07-02 16:54:39 +0300

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# `consul` key/value Repository
+
+To use this repository you need two things:
+
+* An installed [`consul`](https://www.consul.io)  instance.
+ * `brew cask install consul`
+* [`git2consul`](https://github.com/Cimpress-MCP/git2consul) key/value migration tool.
+ * `npm install -g git2consul`
+
+--
+
+Check configurations:
+
+```bash
+$ vi git-and-consul.config
+```
+
+
---
+++ git2consul.sh
@@ -0,0 +1,27 @@
+# Go to current directory
+tmp_dir=$(pwd)
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Load consul configuration
+. ./git-and-consul.config
+
+cat <<EOF > /tmp/git2consul.json
+{
+  "version": "1.0",
+  "repos" : [{
+    "name" : "config",
+    "url" : "${gitRepository}",
+    "source_root": "${gitSourceRoot}",
+    "branches" : ["${gitBranch}"],
+    "hooks": [{
+      "type" : "polling",
+      "interval" : "1"
+    }]
+  }]
+}
+EOF
+
+eval "git2consul --endpoint ${consulHost} --port ${consulPort} --config-file /tmp/git2consul.json"
+
+# Restore previous folder
+cd "$tmp_dir"
